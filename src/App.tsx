ecec1efd42e900a7b998c74424ea40476c3b5b36
skye-lopez <sky.lopez.dev@gmail.com>
2024-07-08 18:55:40 -0700

    Update some naming stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   Flex,
   Spacer,
   Box,
+  Stack,
   Text,
   theme,
   useColorModeValue,
@@ -28,9 +29,10 @@ export const App = () => {
     return (
   <ChakraProvider theme={t}>
     <Flex flexDirection='row' w="100%" h="100%" marginBottom="0">
-        <Box padding="10px">
+        <Stack padding="10px" direction='row'>
             <Text fontSize="2xl" as="b">typee.xyz</Text>
-        </Box>
+            <Text>(v0.0.1 - bugs and low features)</Text>
+        </Stack>
         <Spacer />
         <Box padding="10px">
             <ColorModeSwitcher justifySelf="flex-end" />
